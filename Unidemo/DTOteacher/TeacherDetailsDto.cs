38a3fc8559b9a6880bf27936dbd46810bcdcbcfc
tommi lipponen <lipponen@hotmail.fi>
2022-10-13 18:41:47 +0300

    Properties added

---
+++ Unidemo/DTOteacher/TeacherDetailsDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Unidemo.DTOdepartment;
+using Unidemo.Models;
+
+namespace Unidemo.DTOteacher
+{
+    public class TeacherDetailsDto 
+    {
+       
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string DepartmentName { get; set; }
+
+
+
+    }
+}
